53b151ff44db15a840d6d7c3668ac459356f1276
Dominic Narula <dominic.narula@gmx.de>
2020-01-07 16:12:11 +0100

    first commit

---
+++ src/main/java/de/narula/main/GoGGgfHomepageApplication.java
@@ -0,0 +1,13 @@
+package de.narula.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGGgfHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoGGgfHomepageApplication.class, args);
+	}
+
+}
